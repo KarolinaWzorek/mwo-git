c6ac38da44041342e20170255e012e2f22ec884a
Karolina Wzorek <KarolinaWzorek@agh.pl>
2018-01-28 14:50:39 +0100

    rakiety

--- SomeProgram.java
+++ SomeProgram.java
@@ -2,5 +2,9 @@
         System.out.println("We are learning to use Git.");
 
 System.out.println("We will be Git experts, soon.");
+
+  public static void StartRocket(){
+  System.out.println("ziiiuuuummmm!!!")
+  }
     }
 }
